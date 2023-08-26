7e80276c4dd0a1ca724d4b541bcc60f4c890f311
Nils Emmenegger <61921611+nils-emmenegger@users.noreply.github.com>
2023-08-25 23:00:09 -0400

    Very rough draft of home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,22 @@
 export default function Home() {
   return (
-    <div className="text-onedark-white">
-      <p>home page</p>
+    <div className="mx-auto max-w-3xl p-4">
+      <h1 className="mb-4 text-onedark-light-yellow text-3xl">Hi!</h1>
+      <p className="mb-4 text-base text-onedark-magenta">
+        My name&apos;s Nils Emmenegger. I&apos;m currently studying computer
+        science student at the University of Waterloo, class of 2027.
+      </p>
+
+      <p className="text-base text-onedark-magenta mb-4">
+        On this website, you can find some projects I&apos;ve done, my contact
+        information, and my resume.
+      </p>
+
+      <p className="text-base text-onedark-magenta">
+        In my spare time, you might find me rock climbing, cooking,
+        competitively programming, preaching about memory safety, or playing the
+        VR rhythm game Beat Saber.
+      </p>
     </div>
   );
 }
