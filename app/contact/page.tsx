d02790ef0b359f3be8c56b71e18a6ee91ae8aa48
Nils Emmenegger <61921611+nils-emmenegger@users.noreply.github.com>
2023-08-26 22:38:15 -0400

    Create contact and projects pages

---
+++ app/contact/page.tsx
@@ -0,0 +1,3 @@
+export default function Contact() {
+  return <div className="mx-auto max-w-3xl p-4">Contact page</div>;
+}
---
+++ app/projects/page.tsx
@@ -0,0 +1,3 @@
+export default function Projects() {
+  return <div className="mx-auto max-w-3xl p-4">Projects</div>;
+}
