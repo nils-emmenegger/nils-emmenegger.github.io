496774b2fd2adc86680538b8642cbaf3189e414c
Nils Emmenegger <61921611+nils-emmenegger@users.noreply.github.com>
2023-08-26 22:48:15 -0400

    Add metadata titles to contact and projects pages

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,3 +1,9 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Contact",
+};
+
 export default function Contact() {
   return <div className="mx-auto max-w-3xl p-4">Contact page</div>;
 }
--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,3 +1,9 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Projects",
+};
+
 export default function Projects() {
   return <div className="mx-auto max-w-3xl p-4">Projects</div>;
 }
