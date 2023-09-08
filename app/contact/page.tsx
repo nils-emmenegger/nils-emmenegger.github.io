a6f33149b68ec7458dfedde233c11a410e87c8e2
Nils Emmenegger <61921611+nils-emmenegger@users.noreply.github.com>
2023-09-07 21:26:14 -0400

    Build out contact page

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,3 +1,4 @@
+import ExtLink from "@/components/utils/ExtLink";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
@@ -5,5 +6,30 @@ export const metadata: Metadata = {
 };
 
 export default function Contact() {
-  return <div className="mx-auto max-w-3xl p-4">Contact page</div>;
+  const links: [string, string][] = [
+    ["LinkedIn", "https://www.linkedin.com/in/nils-emmenegger"],
+    ["GitHub", "https://github.com/nils-emmenegger"],
+    ["DMOJ: Modern Online Judge", "https://dmoj.ca/user/Nils_Emmenegger"],
+    ["LeetCode", "https://leetcode.com/nemm/"],
+    ["Codeforces", "https://codeforces.com/profile/nemm"],
+    ["ScoreSaber", "https://scoresaber.com/u/76561198228321708"],
+  ];
+
+  return (
+    <div className="mx-auto max-w-3xl p-4">
+      <h1 className="mb-4 text-onedark-light-yellow text-3xl">Contact</h1>
+
+      <h2 className="mb-4 text-onedark-green text-xl">
+        Some online presences I&apos;ve accumulated throughout the years
+      </h2>
+
+      <ul>
+        {links.map(([name, href]) => (
+          <li key={name} className="mb-2 text-onedark-magenta text-base">
+            {name}: <ExtLink href={href}>{href}</ExtLink>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
