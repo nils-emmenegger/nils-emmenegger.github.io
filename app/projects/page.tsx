2217f4ab94e3cc6992b6f3e3591e76129006daf2
Nils Emmenegger <61921611+nils-emmenegger@users.noreply.github.com>
2023-09-07 21:27:17 -0400

    Build out temporary project page

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,3 +1,4 @@
+import ExtLink from "@/components/utils/ExtLink";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
@@ -5,5 +6,14 @@ export const metadata: Metadata = {
 };
 
 export default function Projects() {
-  return <div className="mx-auto max-w-3xl p-4">Projects</div>;
+  return (
+    <div className="mx-auto max-w-3xl p-4">
+      <h1 className="mb-4 text-onedark-light-yellow text-3xl">Projects</h1>
+
+      <h2 className="mb-4 text-onedark-green text-xl">
+        Coming soon! In the meantime, you can check out my{" "}
+        <ExtLink href="https://github.com/nils-emmenegger">GitHub</ExtLink>.
+      </h2>
+    </div>
+  );
 }
