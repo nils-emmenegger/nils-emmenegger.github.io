78f2b81936ed56545f0a63023118361e2531d9d2
Nils Emmenegger <61921611+nils-emmenegger@users.noreply.github.com>
2025-01-16 02:01:37 -0500

    Add Spinner for loading LeetCode Daily streak

--- components/utils/LCDaily.tsx
+++ components/utils/LCDaily.tsx
@@ -2,8 +2,23 @@
 
 import { useEffect, useState } from "react";
 
+function Spinner() {
+  // "Braille 6 Circle Worm" from https://antofthy.gitlab.io/info/ascii/Spinners.txt
+  const spinnerTickChars = "⠋⠙⠹⠸⠼⠴⠦⠧⠇⠏";
+  const [spinnerIndex, setSpinnerIndex] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setSpinnerIndex((spinnerIndex + 1) % spinnerTickChars.length);
+    }, 75);
+
+    return () => clearInterval(interval);
+  });
+  return <>{spinnerTickChars[spinnerIndex]}</>;
+}
+
 export default function LCDaily() {
-  const [lcStreak, setLcStreak] = useState<string>("");
+  const [lcStreak, setLcStreak] = useState<string>();
   useEffect(() => {
     fetch("https://lc-worker.nilsemmenegger.com/get_lc_daily_streak")
       .then((res) => res.text())
@@ -12,7 +27,8 @@ export default function LCDaily() {
 
   return (
     <p className="text-base text-onedark-magenta">
-      My current LeetCode Daily Challenge streak is: {lcStreak}.
+      My current LeetCode Daily Challenge streak is:{" "}
+      {lcStreak === undefined ? <Spinner /> : lcStreak}.
     </p>
   );
 }
